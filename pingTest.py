c1830a2dcf69d2d8705a620789c3def66c5ac6a1
AiMLOC <66396315+AiMLOC@users.noreply.github.com>
2021-10-29 12:51:04 -0700

    Update pingTest.py

--- pingTest.py
+++ pingTest.py
@@ -24,12 +24,13 @@ def main():
     s.addChannel(s.ROUTING_CHANNEL);
 
     # After sending a ping, simulate a little to prevent collision.
-    s.runTime(1);
-    s.ping(2, 3, "Hello, World");
-    s.runTime(1);
+    s.runTime(100);
 
-    s.ping(1, 10, "Hi!");
-    s.runTime(1);
+    s.ping(9, 5, "Hello World");
+    s.runTime(50);
+
+    s.ping(2, 4, "Hi");
+    s.runTime(50);
 
 if __name__ == '__main__':
     main()
